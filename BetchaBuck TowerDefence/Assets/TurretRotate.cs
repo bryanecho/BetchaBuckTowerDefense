a189d31824a0602df14faf9061e1416e79e0c437
neillyb <neilly_b@hotmail.com>
2015-03-15 01:34:27 -0700

    Less Messy
    -Minor map adjustments
    -Removed unused assets
    -Stubbed Turret Rotation
    -Learned Unity has wonderful auto-path-finding routines... which don't
    work in 2D  :: Solution in next update

---
+++ BetchaBuck TowerDefence/Assets/TurretRotate.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TurretRotate : MonoBehaviour {
+    public float rotationSpeed = 0.5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        this.transform.Rotate(0, 0,rotationSpeed * Time.deltaTime);
+
+	
+	}
+}
